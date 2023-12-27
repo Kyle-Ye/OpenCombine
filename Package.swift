549f6494e65526c9b9323675da174a71d279b049
Kyle <kyle201817146@gmail.com>
2023-12-27 11:54:36 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.7
 
+import Foundation
 import PackageDescription
 
 // This list should be updated whenever SwiftPM adds support for a new platform.
@@ -17,6 +18,54 @@ let supportedPlatforms: [Platform] = [
     .wasi,
 ]
 
+let openCombineTarget: Target = .target(
+    name: "OpenCombine",
+    dependencies: [
+        .target(
+            name: "COpenCombineHelpers",
+            condition: .when(platforms: supportedPlatforms.except([.wasi]))
+        )
+    ],
+    exclude: [
+        "Concurrency/Publisher+Concurrency.swift.gyb",
+        "Publishers/Publishers.Encode.swift.gyb",
+        "Publishers/Publishers.MapKeyPath.swift.gyb",
+        "Publishers/Publishers.Catch.swift.gyb"
+    ],
+    swiftSettings: [
+        .define("WASI", .when(platforms: [.wasi])),
+    ]
+)
+let openCombineFoundationTarget: Target = .target(
+    name: "OpenCombineFoundation",
+    dependencies: [
+        "OpenCombine",
+        .target(
+            name: "COpenCombineHelpers",
+            condition: .when(platforms: supportedPlatforms.except([.wasi]))
+        )
+    ]
+)
+let openCombineDispatchTarget: Target = .target(
+    name: "OpenCombineDispatch",
+    dependencies: ["OpenCombine"]
+)
+
+let openCombineTestsTarget: Target = .testTarget(
+    name: "OpenCombineTests",
+    dependencies: [
+        "OpenCombine",
+        .target(name: "OpenCombineDispatch",
+                condition: .when(platforms: supportedPlatforms.except([.wasi]))),
+        .target(name: "OpenCombineFoundation",
+                condition: .when(platforms: supportedPlatforms.except([.wasi]))),
+    ],
+    swiftSettings: [
+        .unsafeFlags(["-enable-testing"]),
+        .define("WASI", .when(platforms: [.wasi])),
+    ]
+)
+
 let package = Package(
     name: "OpenCombine",
     products: [
@@ -28,29 +77,6 @@ let package = Package(
     targets: [
         .target(name: "COpenCombineHelpers"),
         .target(
-            name: "OpenCombine",
-            dependencies: [
-                .target(name: "COpenCombineHelpers",
-                        condition: .when(platforms: supportedPlatforms.except([.wasi])))
-            ],
-            exclude: [
-                "Concurrency/Publisher+Concurrency.swift.gyb",
-                "Publishers/Publishers.Encode.swift.gyb",
-                "Publishers/Publishers.MapKeyPath.swift.gyb",
-                "Publishers/Publishers.Catch.swift.gyb"
-            ],
-            swiftSettings: [.define("WASI", .when(platforms: [.wasi]))]
-        ),
-        .target(name: "OpenCombineDispatch", dependencies: ["OpenCombine"]),
-        .target(
-            name: "OpenCombineFoundation",
-            dependencies: [
-                "OpenCombine",
-                .target(name: "COpenCombineHelpers",
-                        condition: .when(platforms: supportedPlatforms.except([.wasi])))
-            ]
-        ),
-        .target(
             name: "OpenCombineShim",
             dependencies: [
                 "OpenCombine",
@@ -60,20 +86,10 @@ let package = Package(
                         condition: .when(platforms: supportedPlatforms.except([.wasi])))
             ]
         ),
-        .testTarget(
-            name: "OpenCombineTests",
-            dependencies: [
-                "OpenCombine",
-                .target(name: "OpenCombineDispatch",
-                        condition: .when(platforms: supportedPlatforms.except([.wasi]))),
-                .target(name: "OpenCombineFoundation",
-                        condition: .when(platforms: supportedPlatforms.except([.wasi]))),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-enable-testing"]),
-                .define("WASI", .when(platforms: [.wasi])),
-            ]
-        )
+        openCombineTarget,
+        openCombineFoundationTarget,
+        openCombineDispatchTarget,
+        openCombineTestsTarget,
     ],
     cxxLanguageStandard: .cxx17
 )
@@ -84,4 +100,18 @@ extension Array where Element == Platform {
     func except(_ exceptions: [Platform]) -> [Platform] {
         return filter { !exceptions.contains($0) }
     }
+}
+
+func envEnable(_ key: String) -> Bool {
+    guard let value = ProcessInfo.processInfo.environment[key] else {
+        return false
+    }
+    return value == "1"
+}
+
+let enableCompatibilityTest = envEnable("OPENCOMBINE_COMPATIBILITY_TEST")
+if enableCompatibilityTest {
+    var settings = openCombineTestsTarget.swiftSettings ?? []
+    settings.append(.define("OPENCOMBINE_COMPATIBILITY_TEST"))
+    openCombineTestsTarget.swiftSettings = settings
 }
